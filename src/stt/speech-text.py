82623369f995be295a4a1d67b6cf5ab721f7cfcc
Santiago Henao Rojas <santiago.henaorojas@outlook.com>
2025-11-09 13:39:34 -0500

    tester

--- src/stt/speech-text.py
+++ src/stt/speech-text.py
@@ -1,8 +1,13 @@
 import pyaudio
 from vosk import Model, KaldiRecognizer
 import json
+import os
 
-model = Model("vosk-model-small-en-us-0.15")
+current_dir = os.path.dirname(__file__)
+model_path = os.path.join(current_dir,".../Models/vosk-model-small-en-us-0.15")
+output_dir = os.path.join(current_dir, "../data")
+
+model = Model(model_path)
 rec = KaldiRecognizer(model, 16001)
 
 p = pyaudio.PyAudio()
@@ -30,5 +35,14 @@ except KeyboardInterrupt:
     print("\nFinal Recognized Text: ")
     print(full_text.strip())
 
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, "recognized_text.txt")
     with open("recognized_text.txt", "w") as f:
         f.write(full_text.strip())
+
+finally:
+    try:
+        stream.stop_stream()
+        stream.close()
+    finally:
+        p.terminate()
